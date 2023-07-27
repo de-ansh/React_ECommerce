74e70dd73b19050f755b5387d8e714943419372e
Ashish <anshm9448@gmail.com>
2023-07-27 14:21:49 +0530

    HomePage Design Complete

--- src/customer/components/product/ProductCard.jsx
+++ src/customer/components/product/ProductCard.jsx
@@ -1,27 +1,30 @@
-import React from "react"
-import "./ProductCard.css"
-const ProductCard=()=>{
-    return (
-        <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
-           <div className="h-[20rem]">
-           <img className="h-full w-full object-cover  object-left-top" src="https://rukminim1.flixcart.com/image/612/612/xif0q/sari/v/p/3/free-prathna-om-vastra-fab-unstitched-original-imaghmwuprfjubfn.jpeg?q=70" alt="" />
-           </div>
+import React from "react";
+import "./ProductCard.css";
+const ProductCard = ({ product }) => {
+  return (
+    <div className="productCard w-[15rem] m-3 transition-all cursor-pointer">
+      <div className="h-[20rem]">
+        <img
+          className="h-full w-full object-cover  object-left-top"
+          src={product.imageUrl}
+          alt=""
+        />
+      </div>
 
-           <div className="textpart bg-white p-3 ">
-            <div>
-                <p className="font-bold opaxity-60">PRATHAM BLUE</p>
-                <p className="">Embroidered Bollywood Georgette Saree</p>
-            </div>
-
-            <div className='flex item-center space-x-2'>
-                <p className="font-bold"> ₹789</p>
-                <p className="line-through opacity-50">₹3,999</p>
-                <p className="text-green-600 font-semibold"> 80% off</p>
-            </div>
-
-           </div>
+      <div className="textpart bg-white p-3 ">
+        <div>
+          <p className="font-bold opaxity-60">{product.brand}</p>
+          <p className="">{product.title}</p>
         </div>
-    )
-}
 
-export default ProductCard
+        <div className="flex item-center space-x-2">
+          <p className="font-bold"> {product.discountedPrice}</p>
+          <p className="line-through opacity-50">{product.price}</p>
+          <p className="text-green-600 font-semibold"> {product.discountPersent}% off </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
